fd45197de60aac9f0ac8c4514243094226da3ed6
S Ashwin <47474911+1hef001@users.noreply.github.com>
2020-03-07 22:45:05 +0530

    Add files via upload

--- pypack/main.py
+++ pypack/main.py
@@ -2,11 +2,20 @@ import subprocess
 import sys
 import os
 import importlib
+import pkg_resources
 
 
 def install(pack):
     cmd = ['pip','install','--user']
     cmd.append(pack)
+    execute(cmd)
+    
+def uninstall(pack):
+    cmd = ['pip', 'uninstall', '--yes']
+    cmd.append(pack)
+    execute(cmd)
+
+def execute(cmd):
     result = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     out = result.stdout.readlines()
     for x in out:
@@ -15,30 +24,64 @@ def install(pack):
     for x in out:
         print(x)
 
+
 if __name__ == '__main__':
+    installed = [pkg.key for pkg in pkg_resources.working_set]
     package = sys.argv[1:]
     # print(package)
-    if '-r' in package:
-        NAME = package[-1]
-        f = open(NAME,'r')
-        package = f.readlines()
-        
-        for x in package:
-            install(x)
-        f.close()
-    else:
-        PATH = os.getcwd()
-        im = package[-1]
-        # print(PATH)
-        try:
-            trypack = importlib.import_module(im)
-            print("Module " + im + " exists")
-        except ImportError as e:
-            f = open(PATH + '/requirements.txt','a')
-            f.write(f"{im}\n")
-            install(im)
+    if 'install' in package:
+        if '-r' in package:
+            NAME = package[-1]
+            f = open(NAME,'r')
+            package = f.readlines()
+            for x in package:
+                install(x)
             f.close()
+        else:
+            PATH = os.getcwd()
+            im = package[-1]
+            # print(PATH)
+            if im in installed:
+                print("Module " + im + " exists")
+            else:
+                f = open(PATH + '/requirements.txt','a')
+                f.write(f"{im}\n")
+                install(im)
+                f.close()
 
+    elif 'uninstall' in package:
+        if '-r' in package:
+            NAME = package[-1]
+            f = open(NAME,'r')
+            packages = f.readlines()
+            for x in packages:
+                uninstall(x)
+            f.close()
+            f = open(NAME,'w')
+            f.truncate(0)
+            f.close()
+        else:
+            PATH = os.getcwd()
+            im = package[-1]
+            if im in installed:
+                f = open(PATH + '/requirements.txt','r')
+                packages = f.readlines()
+                f.close()
+                f = open(PATH + '/requirements.txt','w+')
+                # print(packages)
+                for x in packages:
+                    packages[packages.index(x)] = x.strip()
+                uninstall(im)
+                # print(im)
+                packages.remove(im)
+                f.truncate(0)
+                for x in packages:
+                    print(x)
+                    f.write(f"{x}\n")
+                f.close()
+            else:
+                print("Module " + im + " doesn't exist")
+        
 
 
 
